847c1fbb696dcb04207db7ea206d0bfd217d7a27
Noboru Saito <noborusai@gmail.com>
2018-12-27 14:04:22 +0900

    Added title header

--- main.go
+++ main.go
@@ -40,15 +40,16 @@ func getTextComment(i int, token []etree.Token) (text string, comment string) {
 	return strings.TrimLeft(text, " \n"), strings.TrimLeft(comment, "\n")
 }
 
-func xmlParse(root *etree.Element) [][]string {
+func xmlParse(root *etree.Element, filename string) [][]string {
 	var tcList [][]string
 	for i, child := range root.ChildElements() {
 		if child.Tag == "para" {
 			token := child.Child
 			text, comment := getTextComment(i, token)
-			tcList = append(tcList, []string{text, comment})
+			path := filename + " : " + child.GetPath()
+			tcList = append(tcList, []string{text, comment, path})
 		} else {
-			tcList = append(tcList, xmlParse(child)...)
+			tcList = append(tcList, xmlParse(child, filename)...)
 		}
 	}
 	return tcList
@@ -56,42 +57,62 @@ func xmlParse(root *etree.Element) [][]string {
 
 func draw(tcList [][]string) {
 	app := tview.NewApplication()
+	grid := tview.NewGrid().
+		SetRows(2, 0).
+		SetBorders(false)
 	pages := tview.NewPages()
-	for page, h := range tcList {
+	header := tview.NewTextView()
+	header.SetTextAlign(tview.AlignCenter)
+	grid.AddItem(header, 0, 0, 1, 1, 0, 0, false)
+	grid.AddItem(pages, 1, 0, 1, 1, 0, 0, true)
+	for page, tc := range tcList {
+
 		comment := tview.NewTextView().
-			SetTextColor(tcell.ColorGreen).
+			SetTextColor(tcell.ColorGray).
 			SetRegions(true).
 			SetWordWrap(true)
+		comment.SetBorder(false)
+		fmt.Fprintf(comment, tc[1])
+
 		text := tview.NewTextView().
 			SetTextColor(tcell.ColorWhite).
 			SetRegions(false).
 			SetWordWrap(false)
-		comment.SetBorder(false)
 		text.SetBorder(false)
-		fmt.Fprintf(text, h[0])
-		fmt.Fprintf(comment, h[1])
+		fmt.Fprintf(text, tc[0])
+
 		flex := tview.NewFlex().
 			AddItem(text, 0, 1, false).
 			AddItem(comment, 0, 1, true)
 		buf := fmt.Sprintf("page-%d", page+1)
+		title := fmt.Sprintf("%s : %s", buf, tc[2])
 		comment.SetDoneFunc(func(key tcell.Key) {
 			if key == tcell.KeyEnter {
-				pages.SwitchToPage(buf)
+				if pages.HasPage(buf) {
+					pages.SwitchToPage(buf)
+					header.SetText(title)
+				} else {
+					app.Stop()
+				}
 			}
 		})
 		pages.AddPage(fmt.Sprintf("page-%d", page), flex, true, true)
 	}
 	pages.SwitchToPage("page-0")
-	if err := app.SetRoot(pages, true).Run(); err != nil {
+	header.SetText(fmt.Sprintf("page-0 : %s", tcList[0][2]))
+	app.SetFocus(pages)
+
+	if err := app.SetRoot(grid, true).Run(); err != nil {
 		panic(err)
 	}
 }
+
 func main() {
 	doc := etree.NewDocument()
 	if err := doc.ReadFromFile(os.Args[1]); err != nil {
 		panic(err)
 	}
 	// _ = xmlParse(doc.Root())
-	tcList := xmlParse(doc.Root())
+	tcList := xmlParse(doc.Root(), os.Args[1])
 	draw(tcList)
 }
